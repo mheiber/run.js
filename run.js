4f795200c8b840c1c76026f64e6ef29444fc0ca9
mheiber <max.heiber@gmail.com>
2014-09-13 18:07:41 -0400

    Remove run([thisObj],func,funcArgs...) form
    Simplify by removing the "binding" form of
    the run function. Instead, use:
    
    	run(thing.method.bind(thing),funcArgs
    
    Reason:
    Old form did't work when typeof thing
     is 'function'.
    That is: cases in which the func is itself a
     property of a function.
    
    For example, in
    	MongoClient.connect
    
    connect() is a method of MongoClient
    but MongoClient is itself a function

--- run.js
+++ run.js
@@ -1,26 +1,22 @@
 //var Promise=require('es6-promise').Promise;
 var q=require('q');
 
-var _run= function(thisObject, func, funcArgs){
+var _run= function(func, funcArgs){
 	var deferred=q.defer();
 	q.all(funcArgs).then(function(funcArgsValues){
 		funcArgsValues.push(deferred.makeNodeResolver());
-		func.apply(thisObject,funcArgsValues);
+		func.apply(null,funcArgsValues);
 	});
 	return deferred.promise;
 };
 
 var run=function(){
 	// ([thisObj],func,funcArgs...) returns promise
+	//real work is done by _run
 	var args=Array.prototype.slice.call(arguments);
-	var newArgs;
-	//if no obj provided, add null as object to bind to
-	if(typeof args[0] !== 'object') {
-		args.unshift(null);
-	}
 	//arrayify the funcArgs...
-	var newArgs=args.slice(0,2);
-	var tail=args.slice(2);
+	var newArgs=[args[0]]
+	var tail=args.slice(1);
 	newArgs.push(tail);
 
 	log('running '+args[1]+' with args '+tail+'\n');
--- test/test.js
+++ test/test.js
@@ -4,150 +4,62 @@ var run=require('../run.js').run;
 //npm install -g mocha
 //then run "mocha test" in the parent directory
 
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      //expect([1,2,3].length).to.equal(3)
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    })
-  })
-})
-
 describe('run',function(){
-	describe('run without binding',function(){
-		var identity=function(x,callback){
-			callback(null,x);
-		};
-		var simpleArray=run(identity,[1]);
+	var identity=function(x,callback){
+		callback(null,x);
+	};
+	var simpleArray=run(identity,[1]);
 
-		var add1=function(x,callback){
-			callback(null,x+1);
-		};
-		var sum=run(add1,1);
+	var add1=function(x,callback){
+		callback(null,x+1);
+	};
+	var sum=run(add1,1);
 
-		var multiply=function(x,y,callback){
-			callback(null,x*y);
-		};
-
-
-		var productOfVals=run(multiply,2,3);
-		var productOfValAndPromise=run(multiply,sum,3);
-
-		describe('run with single, simple funcArg',function(){
-
-			
-			it('returns a thenable',function(){
-				assert.equal('function',typeof sum.then);
-			});
-
-			it('eventually is result of node func(arg,callback)',function(done){
-				sum.then(function(result){
-					assert.equal(2,result);
-					done()
-				});
-			});
-
+	var multiply=function(x,y,callback){
+		callback(null,x*y);
+	};
+	var productOfVals=run(multiply,2,3);
+	var productOfValAndPromise=run(multiply,sum,3);
+	describe('run with single, simple funcArg',function(){	
+		it('returns a thenable',function(){
+			assert.equal('function',typeof sum.then);
 		});
-
-		describe('run with multiple funcArgs',function(){
-
-			it('returns a thenable',function(){
-				assert.equal('function',typeof sum.then);
-			});
-			it('eventually is result of node func(arg1,arg2,callback)',function(done){
-				productOfVals.then(function(result){
-					assert.equal(6,result);
-					done()
-				});
-			});
-
-		});
-		describe('run with mix of promises and values',function(){
-			it('should eventually be result of node-style func(arg1,arg2Promise,callback)',function(done){
-				productOfVals.then(function(result){
-					assert.equal(6,result);
-					done()
-				});
-			});
-
-		});
-		describe('run(nodeStyleFunc,[x])',function(){
-
-			it('should correctly handle a node-style func that accepts a single array value',function(done){
-				simpleArray.then(function(result){
-					assert.deepEqual([1],result);
-					done()
-				});
+		it('eventually is result of node func(arg,callback)',function(done){
+			sum.then(function(result){
+				assert.equal(2,result);
+				done()
 			});
 		});
 	});
-	describe('run with binding',function(){
-
-		var obj={
-			identity:function(x,callback){
-				callback(null,x);
-			},
-			add1:function(x,callback){
-				callback(null,x+1);
-			},
-			multiply:function(x,y,callback){
-				callback(null,x*y);
-			}
-		};
-
-		var simpleArray=run(obj,obj.identity,[1]);
-		var sum=run(obj,obj.add1,1)
-		var productOfVals=run(obj,obj.multiply,2,3);
-		var productOfValAndPromise=run(obj,obj.multiply,sum,3);
-
-		describe('run with single, simple funcArg',function(){
-			it('returns a thenable',function(){
-				assert.equal('function',typeof sum.then);
-			});
-
-			it('eventually is result of node func(obj,obj.arg,callback)',function(done){
-				sum.then(function(result){
-					assert.equal(2,result);
-					done()
-				});
-			});
-
+	describe('run with multiple funcArgs',function(){
+		it('returns a thenable',function(){
+			assert.equal('function',typeof sum.then);
 		});
-
-		describe('run with multiple funcArgs',function(){
-
-			it('returns a thenable',function(){
-				assert.equal('function',typeof sum.then);
-			});
-			it('eventually is result of node func(obj,obj.arg1,obj.arg2,callback)',function(done){
-				productOfVals.then(function(result){
-					assert.equal(6,result);
-					done()
-				});
-			});
-
-		});
-		describe('run with mix of promises and values',function(){
-			it('should eventually be result of node-style func(obj,obj.arg1,obj.arg2Promise,callback)',function(done){
-				productOfVals.then(function(result){
-					assert.equal(6,result);
-					done()
-				});
-			});
-
-		});
-		describe('run(nodeStyleFunc,[x])',function(){
-
-			it('should correctly handle a node-style func that accepts a single array value',function(done){
-				simpleArray.then(function(result){
-					assert.deepEqual([1],result);
-					done()
-				});
+		it('eventually is result of node func(arg1,arg2,callback)',function(done){
+			productOfVals.then(function(result){
+				assert.equal(6,result);
+				done()
 			});
 		});
-
-
 
 	});
+	describe('run with mix of promises and values',function(){
+		it('should eventually be result of node-style func(arg1,arg2Promise,callback)',function(done){
+			productOfVals.then(function(result){
+				assert.equal(6,result);
+				done()
+			});
+		});
+
+	});
+	describe('run(nodeStyleFunc,[x])',function(){
+
+		it('should correctly handle a node-style func that accepts a single array value',function(done){
+			simpleArray.then(function(result){
+				assert.deepEqual([1],result);
+				done()
+			});
+		});
+	});
+
 });
