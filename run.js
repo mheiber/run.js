82e23fc4071298f65f41fcec1f2651dce0c1f39d
mheiber <max.heiber@gmail.com>
2014-09-13 18:18:22 -0400

    Simplify- eliminate _run function
    Without the need to bind an optional
    thisObj, can simplify everything and
    remove superfluous function.

--- run.js
+++ run.js
@@ -1,34 +1,18 @@
 //var Promise=require('es6-promise').Promise;
 var q=require('q');
 
-var _run= function(func, funcArgs){
+var run=function(func, funcArgs){
+	// (func,funcArgs...) returns promise
+	var args=Array.prototype.slice.call(arguments);
+	//arrayify the funcArgs...
+	var funcArgsArr=args.slice(1);
+
 	var deferred=q.defer();
-	q.all(funcArgs).then(function(funcArgsValues){
+	q.all(funcArgsArr).then(function(funcArgsValues){
 		funcArgsValues.push(deferred.makeNodeResolver());
 		func.apply(null,funcArgsValues);
 	});
 	return deferred.promise;
-};
-
-var run=function(){
-	// ([thisObj],func,funcArgs...) returns promise
-	//real work is done by _run
-	var args=Array.prototype.slice.call(arguments);
-	//arrayify the funcArgs...
-	var newArgs=[args[0]]
-	var tail=args.slice(1);
-	newArgs.push(tail);
-
-	log('running '+args[1]+' with args '+tail+'\n');
-	return _run.apply(null,newArgs);
-};
-
-//set run.logging=false in order to view logs
-log=function(str){
-	if(run.logging){
-		console.log(str);
-	}
-	return this;
 };
 
 exports.run=run;
